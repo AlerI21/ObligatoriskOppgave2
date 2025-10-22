e6d8f16f5eade8979e27367e248c0c30b3d6f3c7
AlerI21 <aleksander.k.e@outlook.com>
2025-10-22 15:41:27 +0200

    .

--- vis-alle-klasser.php
+++ vis-alle-klasser.php
@@ -13,7 +13,7 @@ $antallRader=mysqli_num_rows($sqlResultat); /*Beregner antall rader i resultatet
 
 print ("<h3>Registrerte klasser</h3>");
 print ("<table border=1>");
-print ("<tr><th align=left>klassekode</th> <th align='left'>klassenavn</th> <th align=left>studiumkode</th></tr>")
+print ("<tr><th align=left>klassekode</th> <th align='left'>klassenavn</th> <th align=left>studiumkode</th></tr>");
 
 for ($r=1;$r<=$antallRader;$r++)
 {
