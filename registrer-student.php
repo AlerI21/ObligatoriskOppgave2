c6f2ecc2f19f0857e33c8468327cb00b9fd2a446
AlerI21 <aleksander.k.e@outlook.com>
2025-10-22 16:25:42 +0200

    .

--- registrer-student.php
+++ registrer-student.php
@@ -12,9 +12,17 @@
         Brukernavn <input type="text" id="brukernavn" name="brukernavn" required /> <br/>
         Fornavn <input type="text" id="fornavn" name="fornavn" required /> <br/>
         Etternavn <input type="text" id="etternavn" name="etternavn" required /> <br/>
+        Klassekode
+        <select name="klassekode" id="klassekode">
+            <option value ="">velg klassekode</option>
+            <?php include("dynamiske-funksjoner.php"); listeboksKlassekode(); ?>
         <input type="submit" value="Registrer student" id="registrerStudentKnapp" name="registrerStudentKnapp" /> 
         <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
     </form>
+
+    
+
+
 
     <?php
         if (isset($_POST ["registrerKlasseKnapp"]))
