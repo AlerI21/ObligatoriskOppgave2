c6627b4c7561c77fd30eab15b623661f2225473b
AlerI21 <aleksander.k.e@outlook.com>
2025-10-22 19:18:23 +0200

    .

--- slett-klasse.php
+++ slett-klasse.php
@@ -1,3 +1,12 @@
-<?php 
-	print ("<h3>Slett klasse</h3>"); 
+<?php /*slett-klasse*/
+/*
+/* Programmet lager et skjema for å velge en klasse som skal slettes
+/* Programmet sletter det valgte poststedet*/
+
+<script src="funksjoner.js"> </script>
+
+<h3>Slett klasse</h3>
+
+
+	
 ?> 
