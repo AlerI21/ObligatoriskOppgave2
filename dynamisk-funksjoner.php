6e7edca56cc920df12074b45e7f25d12d3db3413
AlerI21 <aleksander.k.e@outlook.com>
2025-10-23 17:46:00 +0200

    .

--- dynamisk-funksjoner.php
+++ dynamisk-funksjoner.php
@@ -2,18 +2,18 @@
 
 function listeboksKlassekode()
 {
-    include("db-tilkobling.php")  /*tilkobling til database-serveren utført og valg av database foretatt*/
+    include("db-tilkobling.php");  /*tilkobling til database-serveren utført og valg av database foretatt*/
 
     $sqlSetning="SELECT * FROM klasse;";
     $sqlResultat=mysqli_query($db, $sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
     /*SQL-setning sendt til database-serveren*/
 
-    $antallRader=mysqli_num_rows("$sqlResultat");
+    $antallRader = mysqli_num_rows($sqlResultat);
 
     for ($r=1;$r<=$antallRader;$r++)
     {
         $rad=mysqli_fetch_array($sqlResultat);
-        $klassekode=$rad["klassekode"]
+        $klassekode=$rad["klassekode"];
 
          print("<option value='$klassekode'>$klassekode</option>");
     }
